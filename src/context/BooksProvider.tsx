16a82a2c25b06c1bbb8c1e4cb300fc7dd79c600c
Tahera Akter <taherascript@gmail.com>
2026-09-23 15:55:50 +0600

    Book-Vibe project by Next.js

---
+++ src/context/BooksProvider.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { IBook } from "@/type/books.type";
+import { createContext, ReactNode, useState } from "react";
+
+interface IBookContext {
+    readBooks : IBook[],
+    setReadBooks : React.Dispatch<React.SetStateAction<IBook[]>>
+    wishList : IBook[],
+    setWishList : React.Dispatch<React.SetStateAction<IBook[]>>
+
+}
+export const BooksContext = createContext<IBookContext>({
+    readBooks :[],
+    setReadBooks :()=>{},
+    wishList :[],
+    setWishList : ()=>{},
+})
+
+const BooksProvider = ({children} : {children:ReactNode}) => {
+    const [readBooks,setReadBooks] =useState<IBook[]>([])
+    const [wishList,setWishList] = useState<IBook[]>([])
+
+    const sharedData = {
+        readBooks,
+        setReadBooks,
+        wishList,
+        setWishList
+    }
+    return (
+        <BooksContext.Provider value={sharedData} >{children}</BooksContext.Provider>
+    );
+};
+
+export default BooksProvider;
